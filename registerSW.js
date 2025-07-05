b75e47c5bd4c6bb7a4dc26dab1fccb096bc875b3
Harry-0318 <harshitbhandari0318@gmail.com>
2025-07-05 19:12:09 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/discipline-pwa/sw.js', { scope: '/discipline-pwa/' })})}
